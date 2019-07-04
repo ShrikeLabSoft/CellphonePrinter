5d368f2daa6525dfa1d03fc2ac2f8b3860841783
SantiagoMille <santiago.mille96@gmail.com>
2019-07-04 13:19:44 -0400

    New function for switch testing

--- SLA_printer/limit_switch_test.py
+++ SLA_printer/limit_switch_test.py
@@ -2,11 +2,14 @@ import RPi.GPIO as gpio #https://pypi.python.org/pypi/RPi.GPIO more info
 import time
 
 enable_motor = True
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
 
+switchPin = 17
+gpio.setup(switchPin, gpio.IN, pull_up_down = gpio.PUD_UP)  
 
 #Setting up the pins for the stepper motor driver
 def motor_setup():
-    gpio.setmode(gpio.BCM)
     #GPIO23 = Direction
     #GPIO24 = Step
     gpio.setup(23, gpio.OUT)
@@ -18,6 +21,14 @@ def motor_setup():
 #Easydriver has been configured to run at 1/8 microstep
 #Each microstep size is ideally 0.625um
 
+def check_switch():
+    switchVal = gpio.input(switchPin)
+    if(switchVal == True):
+        print('Not pressed')
+        return True
+    else:
+        print("pressed")
+        return False
 
 
 def move_z_axis(distance_mm, speed_mm_s):
@@ -28,7 +39,8 @@ def move_z_axis(distance_mm, speed_mm_s):
     step_counter = int(abs(distance_mm)/0.000625)
     period = float(0.000625/speed_mm_s)
     steps = 0
-    while(step_counter > steps and gpio.input(10)):
+    
+    while(step_counter > steps and check_switch()):#gpio.input(10)):
         gpio.output(24, True)
         gpio.output(24, False)
         steps += 1  
