1c0b9b9213fa145cb3d572994177ea9b718175af
santiagomille <santiago.mille96@gmail.com>
2019-07-20 17:20:14 +0000

    server with serial in tinkerboard

--- SLA_printer/server.py
+++ SLA_printer/server.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-import RPi.GPIO as gpio
 import time
 import sys
-import serial #to allow communication
+import serial 
 
 port = '/dev/ttyACM0'
 
@@ -16,29 +15,6 @@ api = Api(app)
 command = 0
 
 @app.route('/')
-
-def motor_setup():
-	gpio.setmode(gpio.BCM)
-	gpio.setup(18, gpio.OUT)
-	gpio.setup(24, gpio.OUT)
-
-
-def move_z_axis(distance_mm, speed_mm_s):
-	
-	if(int(distance_mm) < 0):
-		gpio.output(18,True)
-	else:
-		gpio.output(18,False)
-	step_counter = float(abs(float(distance_mm))/0.000625)
-	period = float(0.000625/float(speed_mm_s))
-	steps = 0
-	while(step_counter > steps):
-		
-		gpio.output(24,True)
-		gpio.output(24,False)
-		steps += 1
-		time.sleep(period)
-	gpio.output(18,False)
 	
 
 def index():
@@ -48,7 +24,6 @@ def index():
 class ApiCall(Resource):
 	def get(self, distance,speed):
 		print('Distance-> ' + distance + '\n' + 'Speed-> ' + speed + '\n')
-		#move_z_axis(distance,speed)
 
 		command = 2
 		sFA.write((str(command)+'\r').encode())
@@ -56,5 +31,4 @@ class ApiCall(Resource):
 api.add_resource(ApiCall,'/<distance>,<speed>')
 
 if __name__ == '__main__' :
-	#motor_setup()
 	app.run(debug=True,host='0.0.0.0')
